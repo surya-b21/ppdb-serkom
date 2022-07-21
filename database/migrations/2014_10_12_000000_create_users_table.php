ae364d8293d8bdb3e3521c9057419b755609e3f3
surya-b21 <suryabintara80@gmail.com>
2022-07-21 13:25:01 +0700

    menambahkan tema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama');
             $table->string('email')->unique();
+            $table->string('tempat_lahir');
+            $table->timestamp('tanggal_lahir');
+            $table->text('alamat');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_admin');
